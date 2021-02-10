d19e0f403b0e64be336e82d3e39bef9fe8bf2811
AkopovaAnna <anna.akopova.1989@gmail.com>
2021-02-11 00:52:36 +0400

    adding generator for fibonacci

--- homework/iterableFiboncchi.js
+++ homework/iterableFiboncchi.js
@@ -19,7 +19,32 @@ const fib = {
     }
 }
 
+console.log("iterator");
 for (let v of fib) {
     if (v > 10) break;
     console.log(v);
 }
+
+function* genFib(num) {
+
+    let n1 = 0;
+    let n2 = 1;
+    let value = n1 + n2;
+    yield n1;
+    yield n2;
+    while (value <= num) {
+        yield value;
+        n1 = n2;
+        n2 = value;
+        value = n1 + n2;
+    }
+}
+
+console.log("generator");
+let gen = genFib(8);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
